488fea7d54de69f4e19f070ed23b740f8391bdec
Derek sola <Dsola@hawaii.edu>
2020-07-21 13:45:03 -1000

    log-in layouts

---
+++ app/src/main/java/com/example/game/LogIn.java
@@ -0,0 +1,24 @@
+package com.example.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
+
+public class LogIn extends AppCompatActivity {
+    
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        
+        
+    }
+}
