4ec43e4601b14f4b3ab5ba60f538b6a0a2cd6d8f
Derek sola <Dsola@hawaii.edu>
2020-07-21 15:19:24 -1000

    create new account link works

--- app/src/main/java/com/example/game/CreateNewAccount.java
+++ app/src/main/java/com/example/game/CreateNewAccount.java
@@ -1,4 +1,19 @@
 package com.example.game;
 
-public class CreateNewAccount {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class CreateNewAccount extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createnewaccount);
+        
+        
+        
+        
+        
+    }
 }
--- app/src/main/java/com/example/game/LogIn.java
+++ app/src/main/java/com/example/game/LogIn.java
@@ -1,24 +1,43 @@
 package com.example.game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.concurrent.ExecutionException;
 
+
+
 public class LogIn extends AppCompatActivity {
+    
     
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    
+        TextView createNew = (TextView) findViewById(R.id.CreateNewTextView);
+    
+    
+        createNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            openNewActivity();
+            
+            }
+        });
+    
+    
         
-        
+    
+    
+    }
+    
+    public void openNewActivity(){
+        Intent intent = new Intent(this, CreateNewAccount.class);
+        startActivity(intent);
     }
 }
